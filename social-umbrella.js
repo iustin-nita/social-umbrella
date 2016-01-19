59884273bc937eabf160f982f868ed1dcaf0106b
iustinn <iustin.nitza@gmail.com>
2016-01-19 18:08:23 +0200

    image add form

--- social-umbrella.js
+++ social-umbrella.js
@@ -2,32 +2,42 @@
 Images = new Mongo.Collection("images");
 
 if (Meteor.isClient) {
-   Template.images.helpers({images:
-    Images.find({}, {sort:{createdOn: -1, rating:-1}})
-  });
+ Template.images.helpers({images:
+  Images.find({}, {sort:{createdOn: -1, rating:-1}})
+});
 
-   Template.images.events({
-    'click .js-image':function(event){
-        $(event.target).css("width", "50px");
-    }, 
-    'click .js-del-image': function (event) {
-      var image_id = this._id;
-      console.log(image_id);
-      $('#'+image_id).hide('slow', function() {
-        Images.remove({"_id": image_id});
-      });
-    },
-    'click .js-rate-image' : function (event) {
-      var rating = $(event.currentTarget).data('userrating');
-      var image_id = this.id;
-      console.log(image_id);
+ Template.images.events({
+  'click .js-image':function(event){
+    $(event.target).css("width", "50px");
+  }, 
+  'click .js-del-image': function (event) {
+    var image_id = this._id;
+    console.log(image_id);
+    $('#'+image_id).hide('slow', function() {
+      Images.remove({"_id": image_id});
+    });
+  },
+  'click .js-rate-image' : function (event) {
+    var rating = $(event.currentTarget).data('userrating');
+    var image_id = this.id;
+    console.log(image_id);
 
-      Images.update({_id: image_id},
-                    {$set: {rating: rating}}
-                    );
-    }
+    Images.update({_id: image_id},
+      {$set: {rating: rating}}
+      );
+  }
 
-   });
+});
+
+ Template.image_add_form.events({
+  'submit .js-add-image' : function(event) {
+    var img_src, img_alt;
+    img_src = event.target.img_src.value;
+    img_alt = event.target.img_alt.value;
+    console.log('src'+ img_src + 'alt'+img_alt);
+    return false;
+  }
+});
 
 
 
