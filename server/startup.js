b0ee4ae9562b55c3beddca75df5ba51f348b257c
iustinn <iustin.nitza@gmail.com>
2016-01-27 17:14:02 +0200

    added comments and better dummy data

--- server/startup.js
+++ server/startup.js
@@ -1,12 +1,27 @@
 Meteor.startup(function() {
   if (Posts.find().count() === 0) {
     for (var i=1; i<23; i++) {
+      var randomRating = Math.floor(Math.random() * (20 - (-3) + 1)) + (-3);
       Posts.insert(
       {
         source: "img_"+i+".jpg",
         description: "Some short post description"+i,
         addedBy: 'Tiger Woods',
-        rating: 0
+        rating: randomRating,
+        comments: [
+          {
+            commentAuthor: "some guy",
+            commentBody: "Great stuff you posted",
+            commentDate: "01.10.2015"
+          },
+          {
+            commentAuthor: "some other guy",
+            commentBody: "Don't worry, be happy!",
+            commentDate: "01.10.2015"
+          },
+
+        ],
+
       }
       );
     }
