ddd4d882c68e840617c402f5afa884aeb7d4f44e
iustinn <iustin.nitza@gmail.com>
2016-01-27 22:51:30 +0200

    added moment js, format dates

--- client/main.js
+++ client/main.js
@@ -70,6 +70,11 @@ Router.route('/contact', function () {
     passwordSignupFields: 'USERNAME_AND_EMAIL' //  One of 'USERNAME_AND_EMAIL', 'USERNAME_AND_OPTIONAL_EMAIL', 'USERNAME_ONLY', or 'EMAIL_ONLY' (default).
   });
 
+  //global helpers
+  Template.registerHelper('formatDate', function(date) {
+    return moment(date).format('DD-MM-YYYY');
+  });
+
   //body template
   Template.body.helpers({
     username: function() {
