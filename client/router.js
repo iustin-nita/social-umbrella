8742e41d087e7a0fdaba47fab5ba650f8e7fde1b
iustinn <iustin.nitza@gmail.com>
2016-02-10 19:50:48 +0200

    remove comments from router

--- client/router.js
+++ client/router.js
@@ -1,8 +1,3 @@
-//config
-// Router.configure({
-//   layoutTemplate: 'app',
-//   loadingTemplate: 'loading'
-// });
 
 FlowRouter.notFound = {
   action: function() {
@@ -10,11 +5,13 @@ FlowRouter.notFound = {
   }
 };
 
-// Router.route('/', function () {
-//   this.render('hero', {
-//     to: "main"
-//   });
-// });
+FlowRouter.route('/', {
+  name: 'homepage',
+  action: function() {
+    BlazeLayout.render("app", {content: "posts", header: 'navbar'});
+  }
+});
+
 
 FlowRouter.route('/posts', {
   name: 'posts',
@@ -23,69 +20,27 @@ FlowRouter.route('/posts', {
   }
 });
 
-// Router.route('/posts', function () {
-//   this.render('navbar', {
-//     to: "navbar"
-//   });
-//   this.render('posts', {
-//     to: "main"
-//   });
-// });
-
-// Router.route('/add', function () {
-//   this.layout('layout');
-//   this.render('addPostPage');
-//   this.render('navbar', { to: 'navbar'});
-// });
 
 FlowRouter.route('/add', {
   name: 'add',
   action: function() {
-    BlazeLayout.render('app', {content: 'addPostPage'});
+    BlazeLayout.render('app', {content: 'addPostPage', header:'navbar'});
   }
 });
 
-// Router.route('/post/:_id', function () {
-//   this.render('navbar', {
-//     to: "navbar"
-//   });
-//   this.render('post', {
-//     to: "main"
-//   });
-// });
-
-// FlowRouter.route('/post/:id', {
-//   name: 'postPage',
-//   subscriptions: function(params, queryParams) {
-//         this.register('post', Meteor.subscribe('posts', params.postId));
-//     },
-//   action: function(params, queryParams) {
-//     BlazeLayout.render('app', {content: 'post', header:'navbar'});
-//     console.log("Params:", params);
-//         console.log("Query Params:", queryParams);
-//   }
-// });
 
 FlowRouter.route('/post/:_id', {
     name: 'post',
     action: function (params) {
       console.log( params );
-      BlazeLayout.render('app', {content: 'post'});
+      BlazeLayout.render('app', {content: 'post', header:'navbar'});
     }
 });
 
-// Router.route('/contact', function () {
-//   this.render('navbar', {
-//     to: "navbar"
-//   });
-//   this.render('contact', {
-//     to: "main"
-//   });
-// });
 
 FlowRouter.route('/contact', {
   name: 'contact',
   action: function() {
-    BlazeLayout.render('app', {content: 'contact'});
+    BlazeLayout.render('app', {content: 'contact',header:'navbar'});
   }
 });
