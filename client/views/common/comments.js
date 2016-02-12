ea8c4bea37a94d21b648370fc62f42a2ffd9ec09
iustinn <iustin.nitza@gmail.com>
2016-02-12 12:15:33 +0200

    fix comments in all use cases, add some avatars

--- client/views/common/comments.js
+++ client/views/common/comments.js
@@ -1,21 +1,30 @@
 Template.comments.events({
-  'submit .add_comment_form': function() {
-      var currentTarget = event.currentTarget;
-      var commentAuthor = Meteor.user().username;
-      var commentBody = currentTarget.find('.commentBody').val();
-      var post_id = this._id;
-      console.log(currentTarget);
-      var comment = {
+  'submit .add_comment_form': function(event, template) {
+    var currentTarget, commentAuthor, commentBody, post_id,comment;
+    currentTarget = event.target;
+    if (Meteor.user()) {
+      // alert(Meteor.user().profile);
+      if (Meteor.user().profile === undefined) {
+        commentAuthor = Meteor.user().username;
+      } else {
+        commentAuthor = Meteor.user().profile.name;
+      }
+      commentBody = event.target[0].value;
+      post_id = this._id;
+      // console.log(event.target[0].value);
+      comment = {
         commentAuthor: commentAuthor,
         commentBody: commentBody,
         commentDate: new Date()
       };
       console.log(comment);
-      // Posts.update(
-      //   {_id: post_id },
-      //   { $push: { comments: comment }}
-      // );
-
+        Posts.update(
+          {_id: post_id },
+          { $push: { comments: comment }}
+        );
+      } else {
+        Materialize.toast('You need to log in first', 4000);
+      }
       return false;
     }
   });
