348a7a15cb20e29f6e4013b509ff388fbb77f2ef
iustinn <iustin.nitza@gmail.com>
2016-02-17 16:17:25 +0200

    finish edit post template

--- client/views/posts/edit_post.js
+++ client/views/posts/edit_post.js
@@ -38,6 +38,9 @@
   });
 
   Template.editPost.events({
+    'click .delete-post-image': function() {
+
+    },
 
     'click .js-del-post': function (event) {
       var post_id = this._id;
@@ -48,27 +51,53 @@
 
     },
 
-    'click .inactive.like': function(event) {
-      var post_id = this._id;
-      var userId = Meteor.userId();
-      if(userId) {
-        Posts.update( {_id: post_id},
-          {$inc: {likes: +1}, $push: {likers: userId}}
-          );
-        $(this).removeClass('active').addClass('inactive');
-      } else {
-          Materialize.toast('Please log in to like posts', 4000) // 4000 is the duration of the toast
-      }
-
+    'change .post-image': function(event, template) {
+       FS.Utility.eachFile(event, function(file) {
+         Images.insert(file, function (err, fileObj) {
+           if (err){
+              // handle error
+           } else {
+              // handle success depending what you need to do
+             
+             var imagesURL = "/cfs/files/images/" + fileObj._id;
+              Session.set('imageURL', imagesURL);
+           }
+         });
+      });
     },
-    'click .active.like': function(event) {
-      var post_id = this._id;
-      var userId = Meteor.userId();
-      console.log('inactive');
-      Posts.update( {_id: post_id},
-        {$inc: {likes: -1}, $pull: {likers: userId}}
-        );
-      $(this).removeClass('inactive disabled').addClass('active');
+    'submit .js-edit-post' : function(event) {
+      var source, userId, image, description, post;
+      userId = Meteor.userId();
+      description = event.target[0].value;
+        image = Session.get('imageURL');
+        console.log(image);
+        // console.log(event);
+        if (Meteor.user()) {
+          if (description || image) {
+            post = {
+              image: image,
+              description: description,
+              likes: 0,
+              likers: [],
+              commentsCount: 0,
+              };
+              console.log(post);
+            // Meteor.call('insertPost', post, function (error, result) {
+            //     if (error){
+            //       console.log(error.reason);
+            //       sAlert.error("Oops! Something went wrong!"+error.reason+"", {effect:'genie'});
+            //     } else {
+            //       event.target[0].value='';
+            //     }
+            // });
+            event.target[0].value = '';
+          } else {
+            sAlert.info("Please write something cool.", {effect:'genie'});
+          }
+        } else {
+        sAlert.info('You should <a href="/login" class="underline">log in!</a>', {effect:'genie', html: true});
+        }
+      return false;
     },
 
   });
